d0cd8bfe5158a2596e0f051e5410a19cbf5ccd11
icoez <felix@geupel.org>
2023-10-28 12:18:05 +0100

    Added word info API function

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const webster_api_key = '79c9e581-8088-4ce8-9bbb-fa5417a4ed0f'
+
+async function getWordInfo(word) {
+    let definition;
+    let phonetics;
+    let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/sd3/json/${word}?key=${webster_api_key.toLowerCase()}`);
+    let data = await response.json();
+    definition = data[0]['shortdef'][0]
+    phonetics = data[0]['hwi']['prs'][0]['mw']
+    return [definition,phonetics]  
+}   
